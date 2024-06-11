e739280c8655aa87f2058d52ac40a36920d869b7
MEZGHICHE-Ahmed <mezghiche.ahmed@outlook.com>
2024-06-11 14:22:15 +0200

    root
    r

---
+++ controller.py
@@ -0,0 +1,284 @@
+from flask import Flask, render_template, request, redirect, url_for
+from model import Site,Station,Observations
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/corse', methods=['GET', 'POST'])
+def corse():
+    route_path = 'corse'
+    departements = Site.search_departement("CORSE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/centrevdl', methods=['GET', 'POST'])
+def centrevdl():
+    route_path = 'centrevdl'
+    departements = Site.search_departement("CENTRE-VAL DE LOIRE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+
+
+@app.route('/bretagne', methods=['GET', 'POST'])
+def bretagne():
+    route_path = 'bretagne'
+    departements = Site.search_departement("BRETAGNE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/bfc', methods=['GET', 'POST'])
+def bfc():
+    route_path = 'bfc'
+    departements = Site.search_departement("BOURGOGNE-FRANCHE-COMTE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/grandest', methods=['GET', 'POST'])
+def grandest():
+    route_path = 'grandest'
+    departements = Site.search_departement("GRAND EST")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+
+@app.route('/occitanie', methods=['GET', 'POST'])
+def occitanie():
+    route_path = 'occitanie'
+    departements = Site.search_departement("OCCITANIE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/hautsdefr', methods=['GET', 'POST'])
+def hautsdefr():
+    route_path = 'hautsdefr'
+    departements = Site.search_departement("HAUTS-DE-FRANCE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/ara', methods=['GET', 'POST'])
+def ara():
+    route_path = 'ara'
+    departements = Site.search_departement("AUVERGNE-RHONE-ALPES")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+# Continuer pour les autres routes...
+
+@app.route('/normandie', methods=['GET', 'POST'])
+def normandie():
+    route_path = 'normandie'
+    departements = Site.search_departement("NORMANDIE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/pdll', methods=['GET', 'POST'])
+def pdll():
+    route_path = 'pdll'
+    departements = Site.search_departement("PAYS DE LA LOIRE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/paca', methods=['GET', 'POST'])
+def paca():
+    route_path = 'paca'
+    departements = Site.search_departement("PROVENCE-ALPES-COTE D'AZUR")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/idf', methods=['GET', 'POST'])
+def idf():
+    route_path = 'idf'
+    departements = Site.search_departement("ILE-DE-FRANCE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/nouvellea', methods=['GET', 'POST'])
+def nouvellea():
+    route_path = 'nouvellea'
+    departements = Site.search_departement("NOUVELLE-AQUITAINE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/guyane', methods=['GET', 'POST'])
+def guyane():
+    route_path = 'guyane'
+    departements = Site.search_departement("GUYANE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/martinique', methods=['GET', 'POST'])
+def martinique():
+    route_path = 'martinique'
+    departements = Site.search_departement("MARTINIQUE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/mayotte', methods=['GET', 'POST'])
+def mayotte():
+    route_path = 'mayotte'
+    departements = Site.search_departement("MAYOTTE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/guadeloupe', methods=['GET', 'POST'])
+def guadeloupe():
+    route_path = 'guadeloupe'
+    departements = Site.search_departement("GUADELOUPE")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+@app.route('/reunion', methods=['GET', 'POST'])
+def reunion():
+    route_path = 'reunion'
+    departements = Site.search_departement("LA REUNION")
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement.html', departements=departements, route_path=route_path)
+
+
+
+
+
+@app.route('/region', methods=['GET', 'POST'])
+def region():
+    regions = Site.get_all_region()
+    if request.method == 'POST':
+        if "regionss" in request.form:
+            
+            region_name = request.form.get('regionss')
+            return redirect(url_for('departement', region_name=region_name))
+        elif "Commune" in request.form:
+            commune_name = request.form['Commune']
+            
+            return redirect(url_for('search_result', commune_name=commune_name))
+    return render_template('region.html', regions=regions)
+
+@app.route('/search_result/<commune_name>', methods=['GET', 'POST'])
+def search_result(commune_name):
+    result = Station.research_tab(commune_name)
+    if "stationss" in request.form:
+        code_station = request.form.get('stationss')
+        return redirect(url_for('stationsinfo', code_station=code_station))
+    return render_template('search_result.html', result=result)
+
+
+
+@app.route('/departement/<region_name>', methods=['GET', 'POST'])
+def departement(region_name):
+    departements = Site.search_departement(region_name)
+    for i in departements:
+        print(i.libelle_departement)
+    if request.method == 'POST':
+        departement_name = request.form.get('departementss')
+        return redirect(url_for('communes', departement_name=departement_name))
+    return render_template('departement1.html', departements=departements, region_name=region_name)
+
+
+@app.route('/communes/<departement_name>', methods=['GET', 'POST'])
+def communes(departement_name):
+    communes = Site.search_commune(departement_name)
+    if request.method == "POST":
+        commune_name = request.form.get('communess')
+        return redirect(url_for('sites', commune_name=commune_name))
+    return render_template('communes.html', communes=communes,departement_name=departement_name)
+
+@app.route('/sites/<commune_name>', methods=['GET', 'POST'])
+def sites(commune_name):
+    sites = Site.search_sites(commune_name)
+    if request.method == "POST":
+        code_site = request.form.get('sitess')
+        return redirect(url_for('stations', code_site=code_site))
+    return render_template('sites.html', sites=sites,commune_name=commune_name)
+
+@app.route('/stations/<code_site>', methods=['GET', 'POST'])
+def stations(code_site):
+    stations = Station.search_station(code_site)
+
+    if request.method == "POST":
+
+        if "stationss" in request.form:
+            code_station = request.form.get('stationss')
+            return redirect(url_for('stationsinfo', code_station=code_station))
+        
+        elif "station_tr" in request.form:
+            code_station = request.form.get('station_tr')
+            obscheck = Observations.get_obs_tr(code_station)
+            if not obscheck:
+                return redirect(url_for('error404.html'))
+            return redirect(url_for('stations_obs_tr', code_station=code_station))
+        
+        elif "station_date" in request.form:
+            code_station = request.form.get('station_date')
+            return redirect(url_for('stations_obs_date', code_station=code_station, date=None))
+        
+    return render_template('station.html', stations=stations, code_site=code_site)
+
+
+@app.route('/stationsinfo/<code_station>', methods=['GET', 'POST'])
+def stationsinfo(code_station):
+    stationsinfo = Station.get_all_info(code_station)
+    return render_template('stationsinfos.html', stationsinfo=stationsinfo, code_station=code_station)
+
+
+@app.route('/stations_obs_tr/<code_station>', methods=['GET', 'POST'])
+def stations_obs_tr(code_station):
+    observations = Observations.get_obs_tr(code_station)
+    return render_template('station_obs_tr.html', observations=observations, code_station=code_station) 
+
+
+@app.route('/stations_obs_date/<code_station>', methods=['GET', 'POST'])
+def stations_obs_date(code_station):
+    date = request.args.get('date')
+    print(date)
+    observations = Observations.get_obs_date(code_station, date)
+    
+    if request.method == "POST":
+        if "get_date" in request.form:
+            date = request.form.get('date')
+            return redirect(url_for('stations_obs_date', code_station=code_station, date=date))
+    return render_template('station_obs_date.html', observations=observations, date=date, code_station=code_station)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,346 @@
+import requests
+import math
+import datetime
+import matplotlib.pyplot as plt
+import sqlite3
+
+
+DATABASE_FILE = "final\\db_hydro.db"
+
+
+conn = sqlite3.connect(DATABASE_FILE, check_same_thread=False)
+cursor = conn.cursor()
+
+class Observations:
+    def __init__(self, code_station=None, date_debut_serie=None, date_fin_serie=None, date_obs=None, resultat_obs=None, continuite_obs_hydro=None):
+        self.code_station = code_station
+        self.date_debut_serie = date_debut_serie
+        self.date_fin_serie = date_fin_serie
+        self.date_obs = date_obs
+        self.resultat_obs = resultat_obs
+        self.continuite_obs_hydro = continuite_obs_hydro
+
+    def __repr__(self):
+        return (f"Observation(code_station={self.code_station}, date_debut_serie={self.date_debut_serie}, "
+                f"date_fin_serie={self.date_fin_serie}, date_obs={self.date_obs}, "
+                f"resultat_obs={self.resultat_obs}, continuite_obs_hydro={self.continuite_obs_hydro})")
+
+    def get_obs_tr(code_station):
+        url = f'https://hubeau.eaufrance.fr/api/v1/hydrometrie/observations_tr?code_entite={code_station}&pretty&grandeur_hydro=H&fields=code_station,date_debut_serie,date_fin_serie,date_obs,resultat_obs,continuite_obs_hydro&cursor=&size=1'
+        # Télécharger le contenu du fichier JSON
+        response = requests.get(url)
+        
+        # Vérifier que la requête a réussi (code de statut 200)
+        if response.status_code == 206 and Station.station_actif(code_station) == "Actif":
+            # Charger le contenu JSON
+            data = response.json()  # json.loads(response.text) aussi fonctionnera
+            
+            # Accéder à des éléments spécifiques
+            observations = data['data']
+            tab = []
+            for obs in observations:
+                date_debut_serie = obs['date_debut_serie']
+                date_fin_serie = obs['date_fin_serie']
+                date_obs = obs['date_obs']
+                resultat_obs = obs['resultat_obs']
+                continuite_obs_hydro = obs['continuite_obs_hydro']
+            tab.append(Observations(code_station, date_debut_serie, date_fin_serie, date_obs, resultat_obs, continuite_obs_hydro))
+            
+            return tab
+            
+        else:
+            return False
+    
+    def get_obs_date(code_station, date):
+        if date is not None:
+            url = f'https://hubeau.eaufrance.fr/api/v1/hydrometrie/observations_tr?code_entite={code_station}&date_debut_obs={date}&date_fin_obs={date}'
+            # Télécharger le contenu du fichier JSON
+            response = requests.get(url)
+            
+            # Vérifier que la requête a réussi (code de statut 200)
+            if response.status_code in (200, 206):
+                if Station.station_actif(code_station) == "Actif":
+                    # Charger le contenu JSON
+                    data = response.json()  # json.loads(response.text) aussi fonctionnera
+                    tab=[]
+                    # Accéder à des éléments spécifiques
+                    observations = data['data']
+                    if observations:  # Vérifier s'il y a des observations disponibles
+                        obs = observations[0]
+                        date_debut_serie = obs['date_debut_serie']
+                        date_fin_serie = obs['date_fin_serie']
+                        date_obs = obs['date_obs']
+                        resultat_obs = obs['resultat_obs']
+                        continuite_obs_hydro = obs['continuite_obs_hydro']
+                        tab.append(Observations(code_station, date_debut_serie, date_fin_serie, date_obs, resultat_obs, continuite_obs_hydro))
+
+                        return tab
+                    else:
+                        return None
+                else:
+                    return f"La Station est malheureusement {Station.station_actif(code_station)} "
+            else:
+                return f"Impossible d'accéder aux observations de la station"
+        else:
+            return None
+    
+    
+    def get_evol_obs(code_station):
+    # Prendre la date d'aujourd'hui
+        aujourdhui = datetime.date.today()
+        tab_temp = []
+        tab_jour = []
+        
+        # Itérer et imprimer les 30 derniers jours
+        for i in range(1, 30):
+            jour_précédent = aujourdhui - datetime.timedelta(days=i)
+            observation = Observations.get_obs_date(code_station, jour_précédent)
+            
+            if observation is not None:
+                tab_jour.append(i)
+                for i in observation:
+                    tab_temp.append(i.resultat_obs)
+        
+        Observations.create_graph(tab_temp, tab_jour)
+        return tab_temp, tab_jour
+    
+    def create_graph(tab_evol,tab_jour):
+        plt.plot(tab_jour,tab_evol)
+
+        # Step 4: Customize Plot
+        plt.title('Courbe d\'évolution de l\'hydrométrie')
+        plt.xlabel('Nombre de Jours')
+        plt.ylabel('Observations (mesure?)')
+        plt.grid(True)
+
+        # Step 5: Display Plot
+        plt.savefig('static\\graph.png', bbox_inches='tight')
+
+
+class Site:
+
+    #Constructeur pour les détails du site
+    def __init__(self, 
+                code_site=None, 
+                libelle_site=None, 
+                type_site=None, 
+                coordonnee_x_site=None, 
+                coordonnee_y_site=None, 
+                longitude_site=None, 
+                latitude_site=None, 
+                altitude_site=None, 
+                code_systeme_alti_site=None, 
+                libelle_commune=None, 
+                code_departement=None, 
+                code_region=None, 
+                libelle_region=None, 
+                code_cours_eau=None, 
+                grandeur_hydro=None, 
+                date_maj_sites=None, 
+                libelle_departement=None, 
+                number=None):
+        self.code_site = code_site
+        self.libelle_site = libelle_site
+        self.type_site = type_site
+        self.coordonnee_x_site = coordonnee_x_site
+        self.coordonnee_y_site = coordonnee_y_site
+        self.longitude_site = longitude_site
+        self.latitude_site = latitude_site
+        self.altitude_site = altitude_site
+        self.code_systeme_alti_site = code_systeme_alti_site
+        self.libelle_commune = libelle_commune
+        self.code_departement = code_departement
+        self.code_region = code_region
+        self.libelle_region = libelle_region
+        self.code_cours_eau = code_cours_eau
+        self.grandeur_hydro = grandeur_hydro
+        self.date_maj_sites = date_maj_sites
+        self.libelle_departement = libelle_departement
+        self.number = number
+
+    def tout_station_dep(code_departemental):
+        cursor.execute(f"SELECT COUNT(*) FROM Sites where code_departement = {code_departemental}")
+        results = cursor.fetchall()
+
+        return results
+
+    # Preshot : fonction au cas où si on a besoin
+    def get_all_site():
+        cursor.execute("SELECT * FROM Sites")
+        results = cursor.fetchall()
+        tab = []
+
+        for row in results:
+            tab.append(Site(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], 
+            row[8], row[9], row[10], row[11], row[12], row[13], row[14], 
+            row[15],row[16], 0))
+
+        return tab
+    
+    # Preshot : fonction au cas où si on a besoin
+    def get_all():
+        cursor.execute("SELECT * FROM Sites")
+        results = cursor.fetchall()
+        dictio = {}
+
+        for row in results:
+            dictio[Site(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], 
+            row[8], row[9], row[10], row[11], row[12], row[13], row[14], 
+            row[15], row[16], 0)]= Station.get_all_station(row[0])
+        
+        return dictio
+
+    # Affiche les info du site séléctionné
+    def get_site_info(code_site):
+        cursor.execute("SELECT * FROM Sites where code_site = ?", (code_site,))
+        results = cursor.fetchall()
+        tab = []
+
+        for row in results:
+            tab.append(Site(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], 
+            row[8], row[9], row[10], row[11], row[12], row[13], row[14], 
+            row[15],row[16], 0))
+
+        return tab
+
+    def get_all_region():
+        cursor.execute("SELECT DISTINCT libelle_region from sites")
+        result = cursor.fetchall()
+        tab  =[]
+
+        for row in result:
+            if row[0] is not None:
+                tab.append(Site(libelle_region=row[0]))
+
+        return tab
+
+    def search_departement(region):
+        cursor.execute("SELECT DISTINCT sites.libelle_departement, sites.code_departement, COUNT(libelle_station) FROM sites, stations where sites.code_site = stations.code_site AND sites.libelle_region LIKE ? group by libelle_departement ", (region,) )
+        result = cursor.fetchall()
+        tab = []
+
+        for row in result:
+            if row[0] is not None:
+                tab.append(Site(libelle_departement=row[0], code_departement=row[1], number=row[2], libelle_region=region ))
+
+        return tab
+    
+    # Affiche les commune disponibles dans le départements
+    def search_commune(departement):
+        cursor.execute("SELECT DISTINCT sites.libelle_commune, COUNT(libelle_station) FROM sites, stations where sites.code_site = stations.code_site AND sites.libelle_departement LIKE ? group by libelle_station", (departement,) )
+        result = cursor.fetchall()
+        tab = []
+
+        for row in result:
+            if row[0] is not None:
+                tab.append(Site(libelle_commune=row[0], number=row[1], libelle_departement=departement))
+        return tab 
+    
+
+    #Affiche les sites disponible dans la commune
+    def search_sites(commune):
+        cursor.execute("SELECT sites.libelle_site, sites.code_site, COUNT(libelle_station) FROM sites, stations WHERE sites.code_site = stations.code_site and sites.libelle_commune LIKE ? group by sites.libelle_site ", (commune,) )
+        result = cursor.fetchall()
+        tab = []
+        for row in result:
+            if row[0] is not None:
+                tab.append(Site(libelle_site=row[0],code_site=row[1],number=row[2], libelle_commune=commune))
+        return tab
+
+class Station:
+    # Constructeur pour les détails de la station
+    def __init__(self, 
+                 code_station=None, 
+                 code_site=None, 
+                 libelle_site=None, 
+                 libelle_station=None, 
+                 type_station=None, 
+                 coordonnee_x_station=None, 
+                 coordonnee_y_station=None, 
+                 longitude_station=None, 
+                 latitude_station=None, 
+                 altitude_ref_alti_station=None, 
+                 code_commune_station=None, 
+                 libelle_commune=None, 
+                 code_departement=None, 
+                 code_region=None, 
+                 libelle_region=None, 
+                 en_service=None, 
+                 date_fermeture_station=None, 
+                 date_ouverture_station=None):
+        self.code_station = code_station
+        self.code_site = code_site
+        self.libelle_site = libelle_site
+        self.libelle_station = libelle_station
+        self.type_station = type_station
+        self.coordonnee_x_station = coordonnee_x_station
+        self.coordonnee_y_station = coordonnee_y_station
+        self.longitude_station = longitude_station
+        self.latitude_station = latitude_station
+        self.altitude_ref_alti_station = altitude_ref_alti_station
+        self.code_commune_station = code_commune_station
+        self.libelle_commune = libelle_commune
+        self.code_departement = code_departement
+        self.code_region = code_region
+        self.libelle_region = libelle_region
+        self.en_service = en_service
+        self.date_fermeture_station = date_fermeture_station
+        self.date_ouverture_station = date_ouverture_station
+
+
+    def __repr__(self):
+        return (f"{self.code_station} {self.code_site} {self.libelle_site} "
+                f"{self.libelle_station} {self.type_station} "
+                f"{self.coordonnee_x_station} {self.coordonnee_y_station} "
+                f"{self.longitude_station} {self.latitude_station} "
+                f"{self.altitude_ref_alti_station} {self.code_commune_station} "
+                f"{self.libelle_commune} {self.code_departement} "
+                f"{self.code_region} {self.libelle_region} {self.en_service} "
+                f"{self.date_fermeture_station} {self.date_ouverture_station})")
+
+    
+    # Affiche toute les infos du site
+    def get_all_info(code_station):
+        cursor.execute("SELECT * FROM stations where code_station = ?", (code_station,))
+        result = cursor.fetchall()
+        
+        tab = []
+        for row in result:
+            tab.append(Station(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], 
+                row[8], row[9], row[10], row[11], row[12], row[13], row[14], 
+                Station.station_actif(row[0]), row[16], row[17]))
+            
+        return tab
+    
+    # Affiche les stations disponibles disponibles du site
+    def search_station(code_site):
+        cursor.execute("SELECT code_station, libelle_station, type_station FROM stations where code_site = ?", (code_site,))
+        result = cursor.fetchall()
+        tab = []
+
+        for row in result:
+            tab.append(Station(code_station = row[0],libelle_station=row[1],type_station=row[2],en_service=Station.station_actif(row[0])))
+        return tab        
+    
+    def research_tab(nom_ville):
+        like = "%"
+        like += nom_ville + like
+        cursor.execute("SELECT code_station, libelle_station, type_station FROM stations where libelle_commune LIKE ? ", (like,))
+        result = cursor.fetchall()
+        tab = []
+
+        for row in result:
+            tab.append(Station(code_station = row[0],libelle_station=row[1],type_station=row[2],en_service=Station.station_actif(row[0])))
+        return tab   
+
+    def station_actif(code_station):
+        cursor.execute("SELECT en_service, date_fermeture_station FROM stations where code_station = ?", (code_station,))
+        result = cursor.fetchall()
+        for row in result:
+            if row[0] == 0:
+                date_str = row[1]
+                formatted_date_str = date_str.replace("-", "/")
+                return f"Inactif depuis le : {formatted_date_str}"
+            else:
+                return "Actif"
+            
