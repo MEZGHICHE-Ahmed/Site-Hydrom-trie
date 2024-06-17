1ccac299391e38ef6debf28a832300f88b358616
Ousmanesid <147148955+Ousmanesid@users.noreply.github.com>
2024-06-17 20:07:35 +0200

    petite correction d'une exception stats

--- model.py
+++ model.py
@@ -92,7 +92,7 @@ class Observations:
         if response.status_code in (200, 206):
             data = response.json()
             observations = data['data']
-            if observations == []:
+            if observations or data == []:
                 return "Pas de données disponibles"
             else:
                 return observations
